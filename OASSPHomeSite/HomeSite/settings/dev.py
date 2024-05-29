e69d27c76c72797654318cd74831b9be5a948ceb
@josephkb87 <kiyinijoseph@gmail.com>
2024-05-29 17:37:15 +0300

    Add docs

---
+++ OASSPHomeSite/HomeSite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_%cuk37n6!@yb_xwi)3f32*747&cms!=b=1!d#(s-9+7o0p$gu"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
