2e6308417bc2afb8953b4b129b927d9aa3e50e8c
@josephkb87 <kiyinijoseph@gmail.com>
2024-06-05 04:52:30 +0300

    Add models.py

--- OASSPHomeSite/home/models.py
+++ OASSPHomeSite/home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from wagtail.models import Page
 
+
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HomeSite
+
 HomeSite for OASSP
+
+1.[Site Build with Wagtail CMS](https://docs.wagtail.org/en/stable/getting_started/tutorial.html)
+
