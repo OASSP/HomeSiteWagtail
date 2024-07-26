34b4ade224e810b750a7ceafc6eff95a0d29ca0f
clydekingkid <149585931+clydekingkid@users.noreply.github.com>
2024-07-26 19:15:34 +0300

    Update manage.py

--- OASSPHomeSite/manage.py
+++ OASSPHomeSite/manage.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeSite.settings.dev")
 
-    from django.core.management import execute_from_command_line
+    from django.core.management import django
 
-    execute_from_command_line(sys.argv)
+    django(sys.argv)
+    
